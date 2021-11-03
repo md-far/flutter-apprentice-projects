12bf81d453b83222ef8b2cb9128ac8dfc0b92f61
md-far <m.farhan126@gmail.com>
2021-11-03 22:50:17 +0700

    Chapter 3-step 3: create main recipe card, author card, explore card

--- ch3-fooderlich/lib/home.dart
+++ ch3-fooderlich/lib/home.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'card1.dart';
+import 'card2.dart';
+import 'card3.dart';
 
 // 1
 class Home extends StatefulWidget {
@@ -14,12 +17,12 @@ class _HomeState extends State<Home> {
 
   // 8
   static List<Widget> pages = <Widget>[
-    // TODO: Replace with Card1
-    Container(color: Colors.red),
-    // TODO: Replace with Card2
-    Container(color: Colors.green),
-    // TODO: Replace with Card3
-    Container(color: Colors.blue),
+    const Card1(),
+    // Container(color: Colors.red),
+    const Card2(),
+    // Container(color: Colors.green),
+    const Card3(),
+    // Container(color: Colors.blue),
   ];
 
   // 9
@@ -34,37 +37,35 @@ class _HomeState extends State<Home> {
     return Scaffold(
       appBar: AppBar(
           title: Text(
-            'Fooderlich',
-            // 2
-            style: Theme.of(context).textTheme.headline6,
-          )
-      ),
+        'Fooderlich',
+        // 2
+        style: Theme.of(context).textTheme.headline6,
+      )),
       body: pages[_selectedIndex],
       // 4
       bottomNavigationBar: BottomNavigationBar(
-        // 5
-        selectedItemColor:
-          Theme.of(context).textSelectionTheme.selectionColor,
-        // 10
+          // 5
+          selectedItemColor:
+              Theme.of(context).textSelectionTheme.selectionColor,
+          // 10
           currentIndex: _selectedIndex,
-        // 11
+          // 11
           onTap: _onItemTapped,
-        // 6
-        items: <BottomNavigationBarItem>[
-          const BottomNavigationBarItem(
-            icon: Icon(Icons.card_giftcard),
-            label: 'Card',
-          ),
-          const BottomNavigationBarItem(
-            icon: Icon(Icons.card_giftcard),
-            label: 'Card2',
-          ),
-          const BottomNavigationBarItem(
-            icon: Icon(Icons.card_giftcard),
-            label: 'Card3',
-          ),
-        ]
-      ),
+          // 6
+          items: <BottomNavigationBarItem>[
+            const BottomNavigationBarItem(
+              icon: Icon(Icons.card_giftcard),
+              label: 'Card',
+            ),
+            const BottomNavigationBarItem(
+              icon: Icon(Icons.card_giftcard),
+              label: 'Card2',
+            ),
+            const BottomNavigationBarItem(
+              icon: Icon(Icons.card_giftcard),
+              label: 'Card3',
+            ),
+          ]),
     );
   }
 }
